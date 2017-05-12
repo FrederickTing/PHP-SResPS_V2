13dd683d26f2b6c9f369b32dd724612618bee81a
Fang Siang <100060266@students.swinburne.edu.my>
2017-05-12 11:21:20 +0800

    Add files via upload

--- export.php
+++ export.php
@@ -6,8 +6,8 @@ if(isset($_POST["export"]))
     header('Content-Disposition: attachment; filename=data.csv');
     
     $output= fopen("php://output","w");
-    fputcsv($output,array('Customer Name','Date','Sales ID','Total Quantity'));
-    $query= "SELECT * FROM sold ORDER BY cust_id DESC";
+    fputcsv($output,array('Sales ID','Sales Name','Sales Date'));
+    $query="SELECT * FROM sales ORDER BY sales_id DESC";
     $result = mysqli_query($connect,$query);
     while($row = mysqli_fetch_assoc($result))
     {
